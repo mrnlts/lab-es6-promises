38ff5beed1e2482e3db6e2dcd3017cc9d2937e2f
marina <marinaaltesmagret@gmail.com>
2021-04-04 17:51:15 +0200

    Bonus 1

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,5 @@
+const table = document.getElementById( "table" );
+
 // Iteration 0
 
 // function addFood( recipe, recipeID ) 
@@ -29,79 +31,99 @@
 
 
 
-// // Iteration 1 using callbacks
-// addFood(steak[0], '#steak', () => {
-  
-//   addFood( steak[1], '#steak', () => {
-    
-//     addFood( steak[2], '#steak', () => {
-      
-//       addFood( steak[3], '#steak', () => {
-        
-//         addFood( steak[4], '#steak', () => {
-          
-//           addFood( steak[5], '#steak', () => {
+// Iteration 1 using callbacks
+let steakImg = document.createElement( "div" );
+steakImg.innerHTML = ' <img src = "./public/images/steak.jpg"> ';
 
-//             addFood( steak[6], '#steak', () => {
+addFood(steak[0], '#steak', () => {
+  
+  addFood( steak[1], '#steak', () => {
+    
+    addFood( steak[2], '#steak', () => {
+      
+      addFood( steak[3], '#steak', () => {
+        
+        addFood( steak[4], '#steak', () => {
+          
+          addFood( steak[5], '#steak', () => {
+
+            addFood( steak[6], '#steak', () => { 
               
-//               addFood( steak[7], '#steak', () => { 
+              addFood( steak[7], '#steak', () => { 
 
-//               });
+                table.appendChild( steakImg );
+
+              });
                           
-//             });
+            });
           
-//           });
+          });
         
-//         });
+        });
       
-//       });
+      });
     
-//     });
+    });
   
-//   });
+  });
 
-// });
-
-
+});
 
 
-// // Iteration 2 using `.then()`
-// addFood( mashPotatoes[0], '#mashPotatoes')
 
-//   .then( () => {
+
+// Iteration 2 using `.then()`
+let potatoesImg = document.createElement( "div" );
+
+potatoesImg.innerHTML = ' <img src = "./public/images/mashPotatoes.jpg"> ';
+
+addFood( mashPotatoes[0], '#mashPotatoes')
+
+  .then( () => {
   
-//     addFood( mashPotatoes[1], '#mashPotatoes')
-//       .then( () => {
+    addFood( mashPotatoes[1], '#mashPotatoes')
+      .then( () => {
     
-//         addFood( mashPotatoes[2], '#mashPotatoes')
-//           .then( () => {
+        addFood( mashPotatoes[2], '#mashPotatoes')
+          .then( () => {
         
-//           addFood( mashPotatoes[3], '#mashPotatoes')
-//             .then( () => {
+          addFood( mashPotatoes[3], '#mashPotatoes')
+            .then( () => {
           
-//               addFood( mashPotatoes[4], '#mashPotatoes');
-        
-//           });
-    
-//       });
-  
-//     });
+              addFood( mashPotatoes[4], '#mashPotatoes')
+                .then( () => {
 
-//   });
+                  table.appendChild( potatoesImg );
+
+              });
+      
+          });
+  
+        });
+
+    });
+
+});
 
 
 
 
 // Iteration 3 using async and await
 
-  async function makeFood( steps, id ) {
-  
-    for ( let i = 0; i < brusselSprouts.length; i++ ) {
+let brusselSproutsImg = document.createElement( "div" );
 
-      await addFood( steps[i], '#brusselSprouts' );
+brusselSproutsImg.innerHTML = ' <img src = "./public/images/brusselSprouts.jpg"> ';
 
-    }
+async function makeFood( steps, id ) {
+
+  for ( let i = 0; i < brusselSprouts.length; i++ ) {
+
+    await addFood( steps[i], '#brusselSprouts' );
 
   }
 
-  makeFood( brusselSprouts );
+  table.appendChild(brusselSproutsImg);
+
+}
+
+makeFood( brusselSprouts );
